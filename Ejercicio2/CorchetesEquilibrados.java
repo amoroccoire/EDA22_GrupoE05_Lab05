ad7f35596d26bcef50c271df5eea25ab38c431a3
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-06-29 02:23:06 -0500

    Se agrego hashMap con el fin de reducir muchos if

--- Ejercicio2/CorchetesEquilibrados.java
+++ Ejercicio2/CorchetesEquilibrados.java
@@ -1,6 +1,7 @@
 package Ejercicio2;
 
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class CorchetesEquilibrados {
 
@@ -13,15 +14,21 @@ public class CorchetesEquilibrados {
 
     public static String isBalanced(String cadena) {
         Stack<Character> pila = new Stack<Character>();
+        HashMap<Character, Character> parejas = new HashMap<Character, Character>();
+        parejas.put(')', '(');
+        parejas.put(']', '[');
+        parejas.put('}', '{');
 
         for (int i = 0; i < cadena.length(); i++) {
             char simbolo = cadena.charAt(i);
 
             if (simbolo == '[' || simbolo == '(' || simbolo == '{')
                 pila.push(simbolo);
-            else if (simbolo == ']' && pila.peek() == '['){
-                pila.pop();
+            else {
+                if (parejas.get(simbolo) == pila.peek())
+                    pila.pop();
+                else
+                    return "NO";
             }
-        }
     }
 }
